eb63ccc5948907edf4d28f4ca8301c68e0f7cee4
Esdras Tavares da Silva <esdrastavaresdasilva@gmail.com>
2023-03-14 15:24:50 -0300

    config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import serverless from 'serverless-http';
 import app from './app';
-import { PORT } from './config/env';
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
 
 module.exports.handler = serverless(app);
